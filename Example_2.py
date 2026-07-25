91b395486d737a1c7f47d5d0ecaeec63bdabfac6
Nandini503628 <nandaninarang9@gmail.com>
2026-07-25 08:51:49 +0530

    Update Example_2.py

--- Example_2.py
+++ Example_2.py
@@ -3,7 +3,7 @@ from tkinter import messagebox
 import requests
 def get_weather():
     city=city_entry.get()
-    api_key="81986ee77ee1487279a15d9e67c6e53b"
+    api_key=""
     url="https://api.openweathermap.org/data/2.5/weather?"
     complete_url=url+"appid="+api_key+"&q="+city
     res=requests.get(complete_url)
